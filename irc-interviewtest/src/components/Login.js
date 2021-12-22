b4e4a935bfc30360dc92ccfc5800ce3983be8740
Jonathan <jonsant0s@outlook.com>
2021-12-22 15:19:58 -0700

    Expanded Login Handlers

--- irc-interviewtest/src/components/Login.js
+++ irc-interviewtest/src/components/Login.js
@@ -1,49 +1,60 @@
 import { useState } from 'react';
 
-
 export const Login = () => {
+
     const [loginValues, setLoginValues] = useState({
-        username: "",
+        email: "",
         password: "",
+        remember_me: false,
     });
 
     const handleInputChange = (e) => {
         e.persist();
         let prev = { ...loginValues };
-        prev[e.target.id] = e.target.value;
+        prev[e.target.id] = e.target.type === "checkbox" ? e.target.checked : e.target.value;
 
         setLoginValues(prev);
     }
 
     const handleLogin = (e) => {
-        
+        e.preventDefault();
+        alert("Clicked "+loginValues.email + " " + loginValues.password + " " + loginValues.remember_me);
     }
 
 
     return (
         <div>
-            <form id="Login">
+            <form id="Login" onSubmit={handleLogin}>
                 <label>Email</label>
                 <input
                     type="email"
-                    name="login_email"
-                    id="login_email"
+                    name="email"
+                    id="email"
+                    onChange={handleInputChange}
                 />
                 <label>Password</label>
                 <input
                     type="password"
-                    name="login_password"
-                    id="login_password"
+                    name="password"
+                    id="password"
+                    onChange={handleInputChange}
                 />
                 <input
                     type="checkbox"
                     name="remember_me"
                     id="remember_me"
+                    onChange={handleInputChange}
                 />
                 <label>Remember Me</label>
+                <button onClick={handleLogin}> Continue </button>
             </form>
             <div>
                 <a href="#">Forgot password?</a>
+            </div>
+            <div>
+                <a href="#">Contact Us</a>
+                <a href="#">Privacy Policy</a>
+                <a href="#">Terms of Services</a>
             </div>
         </div>
     )
